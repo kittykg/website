ebb90e6837a7ad959e251b56f1de41216a04cd51
Kitty Gu <kexinkittygu@gmail.com>
2021-07-26 15:30:11 +0100

    MVP done

--- pages/matt-kitty-wedding.vue
+++ pages/matt-kitty-wedding.vue
@@ -4,11 +4,15 @@
       Please join our wedding ceremony at Christ Church Kensington,<br>
       on 28 Aug 2021 14:00 BST, 21:00 CST.<br> <br>
       Click the RSVP button to tell us that you would like to come,<br>
-      either in-person or virtually.<br>
-      We would send out the invitations in the next couple of weeks.
+      either in-person or virtually.
     </h3>
 
     <v-btn outlined class="rsvp-btn my-2" v-on:click="rsvp">RSVP</v-btn>
+
+    <h3 class="my-4">
+      If you'd like to give us a small gift, please check out the list
+      <a href="/wedding-gift-list">here</a>.
+    </h3>
   </v-container>
 </template>
 
